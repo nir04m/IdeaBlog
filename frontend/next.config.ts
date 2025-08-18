550aacbefea799e17abb24d5034ff077ce4baa02
Miro <mairoenwa@gmail.com>
2025-08-17 18:11:55 -0600

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   eslint: { ignoreDuringBuilds: true },
+
+  async redirects() {
+    return [
+      {
+        source: "/",          // when visiting /
+        destination: "/landing", // send them here
+        permanent: true,      // 308 permanent redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
